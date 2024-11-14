597a6e64da4e588c0faaf8f701023cf9015133d3
Aletdev <alejandrortsinani@outlook.com>
2024-11-14 06:44:12 -0400

    Commit final del curso

---
+++ src/main/java/com/ale/springboot/di/app/springboot_di/repositories/ProductRepositoryJson.java
@@ -0,0 +1,49 @@
+package com.ale.springboot.di.app.springboot_di.repositories;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.ale.springboot.di.app.springboot_di.models.Product;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ProductRepositoryJson implements IProductRepository {
+
+    private List<Product> list;
+
+
+    public ProductRepositoryJson() {
+        Resource resource = new ClassPathResource("json/product.json");
+        readValueJson(resource);
+    }
+
+    public ProductRepositoryJson(Resource resource) {
+        readValueJson(resource);
+    }
+
+    private void readValueJson(Resource resource) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            list = Arrays.asList(objectMapper.readValue(resource.getInputStream(), Product[].class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return list;
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return list.stream().filter(p -> {
+            return p.getId().equals(id);
+        }).findFirst().orElseThrow();
+    }
+
+}
--- src/main/java/com/ale/springboot/di/app/springboot_di/services/ProductServiceImpl.java
+++ src/main/java/com/ale/springboot/di/app/springboot_di/services/ProductServiceImpl.java
@@ -3,37 +3,36 @@ package com.ale.springboot.di.app.springboot_di.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.ale.springboot.di.app.springboot_di.models.Product;
 import com.ale.springboot.di.app.springboot_di.repositories.IProductRepository;
 
 @Service
-public class ProductServiceImpl implements IProductService{
-    
-    @Autowired
-    //@Qualifier("productFoo")
+public class ProductServiceImpl implements IProductService {
+
+    @Value("${config.price.tax}")
+    private Double tax;
+
     private IProductRepository repository;
 
-    // public ProductServiceImpl(@Qualifier("productList")IProductRepository repository) {
-    //     this.repository = repository;
-    // }
+    public ProductServiceImpl(@Qualifier("productJson") IProductRepository repository) {
+        this.repository = repository;
+    }
 
     public List<Product> findAll() {
-        return repository.findAll().stream().map(p ->{
-            Double priceTax = p.getPrice() * 1.25d;
-            //Product newProd = new Product(p.getId(),p.getName(),priceImp.longValue());
+        return repository.findAll().stream().map(p -> {
+            Double priceTax = p.getPrice() * tax;
             Product newProd = (Product) p.clone();
             newProd.setPrice(priceTax.longValue());
             return newProd;
         }).collect(Collectors.toList());
     }
-  
+
     public Product findById(Long id) {
         return repository.findById(id);
     }
-
 
 }
