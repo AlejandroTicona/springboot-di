45d8735d95567306cb3f2d745013cfdbc91241c9
Aletdev <alejandrortsinani@outlook.com>
2024-11-14 06:44:48 -0400

    Commit final del curso

--- src/main/java/com/ale/springboot/di/app/springboot_di/repositories/ProductRepositoryImpl.java
+++ src/main/java/com/ale/springboot/di/app/springboot_di/repositories/ProductRepositoryImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.context.annotation.RequestScope;
 
 import com.ale.springboot.di.app.springboot_di.models.Product;
 
--- src/main/java/com/ale/springboot/di/app/springboot_di/repositories/ProductRepositoryJson.java
+++ src/main/java/com/ale/springboot/di/app/springboot_di/repositories/ProductRepositoryJson.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
